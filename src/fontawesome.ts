3f26b5150eccbd89b57a68057127a71db6984dbf
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-04 19:58:54 +0100

    fontawesome imports

--- src/fontawesome.ts
+++ src/fontawesome.ts
@@ -1,5 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { faAws, faCss3Alt, faDocker, faGithub, 
+    faHtml5, faJs, faLaravel, faLinux, 
+    faNodeJs, faPhp, faReact, faTrello, faVuejs, fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fab, faCheckSquare, faCoffee, faMoon, faSun)
+library.add(fab, faGithub, faReact, faNodeJs, faJs, faDocker, faAws, faPhp, 
+    faHtml5, faCss3Alt, faTrello, 
+    faLinux, faVuejs, faLaravel)
