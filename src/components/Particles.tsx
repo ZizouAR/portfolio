9aeb0c7cc3b6cc9e1913e68320444bd9d0c5c127
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-04 18:42:07 +0100

    background particles compoonent

---
+++ src/components/Particles.tsx
@@ -0,0 +1,81 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import { useTheme } from "../contexts/Theme";
+import { ThemeMode } from "../types/IThemeContext";
+
+export default function ParticlesComponent() {
+    const { theme } = useTheme()
+
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 10,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: theme === ThemeMode.dark
+                            ? "#ffffff"
+                            : "#000000",
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: true,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 30,
+                    },
+                    opacity: {
+                        value: 0.2,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 6 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
