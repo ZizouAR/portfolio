d2bf8fbcb513f61805ceaf4052c2207a2b2ca8ad
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-04 19:58:41 +0100

    technologies icons map

---
+++ src/data/technologies.ts
@@ -0,0 +1,59 @@
+export type Icon = {
+    name: string,
+    color: string
+}
+
+export const TECHNOLOGIES: Icon[] = [
+    {
+        name: 'html5',
+        color: '#D36A39'
+    },
+    {
+        name: 'css3-alt',
+        color: '#4162E2'
+    },
+    {
+        name: 'js',
+        color: '#E9D84D'
+    },
+    {
+        name: 'node-js',
+        color: '#91B154'
+    },
+    {
+        name: 'react',
+        color: '#87D1EF'
+    },
+    {
+        name: 'vuejs',
+        color: '#69AF83'
+    },
+    {
+        name: 'php',
+        color: '#7577AA'
+    },
+    {
+        name: 'laravel',
+        color: '#E44230'
+    },
+    {
+        name: 'docker',
+        color: '#3D63E5'
+    },
+    {
+        name: 'aws',
+        color: '#EC9D37'
+    },
+    {
+        name: 'linux',
+        color: '#1C1C1C'
+    },
+    {
+        name: 'github',
+        color: '#1C1C1C'
+    },
+    {
+        name: 'trello',
+        color: '#3158C9'
+    },
+] as Icon[]
