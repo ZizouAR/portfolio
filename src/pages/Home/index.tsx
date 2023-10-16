6eb042b3f822d37741eeecf590739c9ab7265f3d
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-16 11:24:06 +0100

    Trim main text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,12 +4,11 @@ import { getAge } from '../../helpers/getAge'
 import { ThemeMode } from '../../types/IThemeContext'
 import { useTheme } from '../../contexts/Theme'
 import MagicFeather from '../../components/MagicFeather'
+import Button from '../../components/Button'
 
 export default function Home() {
 
    const { theme } = useTheme()
-
-
 
    return (
       <div className="main">
@@ -18,7 +17,6 @@ export default function Home() {
                <h1 className="grand-title"><MagicFeather text='Hey! I am Zineddine' /></h1>
                <h2 className="grand-subtitle"><MagicFeather text='a fullstack Web & Mobile developer' startIn={1500} /></h2>
                <p className="caption">
-
                   I'm {getAge()} and I've been designing, developing and putting applications into production for companies for a few years now.
                   I also have experience in team and project management. I'm a fan of Typescript and JS frameworks, but not just that...
                </p>
@@ -34,8 +32,13 @@ export default function Home() {
                            : icon.color} size='2x' />
                   )}
                </div>
+               <Button>
+                  Let's see more
+               </Button>
             </div>
-
+         </section>
+         <section>
+            
          </section>
       </div>
    )
