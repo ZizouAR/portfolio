39588826c3e95bbc5aac61fefd6eec4d803447e4
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-18 19:04:06 +0100

    project data

---
+++ src/data/projects.ts
@@ -0,0 +1,131 @@
+import { Icon } from "./technologies";
+
+export default interface IProject {
+    title: string;
+    description: string;
+    thumbnail: string;
+    technologies: Icon[]
+}
+
+export const Projects: IProject[] = [
+    {
+        title: 'Flex Go',
+        description: `FlexGo is a VTC application that allows users to order a driver in real time to take them to their destination.
+        I designed the driver application and all the backend logic for the application during this project.`,
+        thumbnail: 'images/projects/flexgo/thumbnail.png',
+        technologies: [
+            {
+                name: 'js',
+                color: '#E9D84D'
+            },
+            {
+                name: 'node-js',
+                color: '#91B154'
+            },
+            {
+                name: 'react',
+                color: '#87D1EF'
+            },
+            {
+                name: 'docker',
+                color: '#3D63E5'
+            },
+            {
+                name: 'aws',
+                color: '#EC9D37'
+            },
+            {
+                name: 'linux',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'github',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'trello',
+                color: '#3158C9'
+            },
+        ]
+    },
+    {
+        title: 'Flex Go',
+        description: `FlexGo is a VTC application that allows users to order a driver in real time to take them to their destination.
+        I designed the driver application and all the backend logic for the application during this project.`,
+        thumbnail: 'images/projects/flexgo/thumbnail.png',
+        technologies: [
+            {
+                name: 'js',
+                color: '#E9D84D'
+            },
+            {
+                name: 'node-js',
+                color: '#91B154'
+            },
+            {
+                name: 'react',
+                color: '#87D1EF'
+            },
+            {
+                name: 'docker',
+                color: '#3D63E5'
+            },
+            {
+                name: 'aws',
+                color: '#EC9D37'
+            },
+            {
+                name: 'linux',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'github',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'trello',
+                color: '#3158C9'
+            },
+        ]
+    },
+    {
+        title: 'Flex Go',
+        description: `FlexGo is a VTC application that allows users to order a driver in real time to take them to their destination.
+        I designed the driver application and all the backend logic for the application during this project.`,
+        thumbnail: 'images/projects/flexgo/thumbnail.png',
+        technologies: [
+            {
+                name: 'js',
+                color: '#E9D84D'
+            },
+            {
+                name: 'node-js',
+                color: '#91B154'
+            },
+            {
+                name: 'react',
+                color: '#87D1EF'
+            },
+            {
+                name: 'docker',
+                color: '#3D63E5'
+            },
+            {
+                name: 'aws',
+                color: '#EC9D37'
+            },
+            {
+                name: 'linux',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'github',
+                color: '#1C1C1C'
+            },
+            {
+                name: 'trello',
+                color: '#3158C9'
+            },
+        ]
+    }
+]
