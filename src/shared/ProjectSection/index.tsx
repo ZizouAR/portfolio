70a5e832215ef05563931d4f2a10963af4d851f7
Zineddine Ait Rabah <zioustan.aitrabah@gmail.com>
2023-10-19 18:29:03 +0100

    reveal elements on scroll using frame motion

--- src/shared/ProjectSection/index.tsx
+++ src/shared/ProjectSection/index.tsx
@@ -1,39 +1,50 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Icon } from "../../data/technologies";
-import { useTheme } from "../../contexts/Theme";
-import { ThemeMode } from "../../types/IThemeContext";
+import { useEffect, useRef } from "react";
 import IProject from "../../data/projects";
-
+import { motion, useAnimation, useInView } from 'framer-motion'
+import Icon from "../../components/Icon";
+import { IconType } from "../../data/technologies";
 export interface IProjectSection extends IProject {
     reversed: boolean;
 }
 
 export default function ProjectSection({ title, thumbnail, description, technologies, reversed }: IProjectSection) {
 
-    const { theme } = useTheme();
+    const controls = useAnimation();
+    const ref = useRef<any>(null);
+
+    const isInView = useInView(ref)
+
+    useEffect(() => {
+        if (isInView) {
+          controls.start("visible");
+        }
+      }, [controls, isInView]);
+
     return (
-        <div className='project-container' style={{ flexDirection: reversed ? 'row-reverse' : 'row' }}>
-            <div>
+        <motion.div
+            initial="hidden"
+            animate={controls}
+            ref={ref}
+            variants={{
+                hidden: { opacity: 0, y: 75 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            transition={{ duration: 1, delay: 0.25 }}
+            className='project-container'
+            style={{ flexDirection: reversed ? 'row-reverse' : 'row' }}>
+
+            <div className="project-description">
                 <h2 className='grand-subtitle'>{title}</h2>
                 <p className="caption project-description">
                     {description}
                 </p>
                 <div className='technologies'>
-                    {technologies.map((icon: Icon, index) =>
-                        <FontAwesomeIcon
-                            //@ts-ignore
-                            icon={`fa-brands fa-${icon.name}`}
-                            key={index}
-                            color={icon.color === '#1C1C1C'
-                                && theme === ThemeMode.dark
-                                ? '#FFFFFF'
-                                : icon.color} size='2x' />
-                    )}
+                    {technologies.map((icon: IconType, index) => <Icon {...icon} key={index} /> )}
                 </div>
             </div>
-            <div>
-                <img src={thumbnail} width="200"  />
+            <div className="project-thumbnail">
+                <img src={thumbnail} width="200" />
             </div>
-        </div>
+        </motion.div>
     )
 }
